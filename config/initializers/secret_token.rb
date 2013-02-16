ebc70784215229aa12e489afb6b957f3186ea2bc
Andrew St. Angelo <astangel@iupui.edu>
2013-02-16 15:21:39 -0500

    Assingment 6 - Init (pre-UserSession)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Propertymanagement::Application.config.secret_token = 'a0642bf0cebdda4b0bf8acd0687d5cf757fdc29afaaa78be759fe605ff831555878a6f089158bd1be684063b4f566e12925a39c1a6226949842e82eb88d1caa6'
